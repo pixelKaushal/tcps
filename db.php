b4378c54d890f4faac39e1b71ccf2bf203ddeb33
Kaushal Gautam <gautamkaushal381@gmail.com>
2025-10-04 20:29:26 +0545

    Update db.php

--- db.php
+++ db.php
@@ -50,8 +50,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $stmt->close();
 }
 // Insert default admin safely
-$adminUser = "yourusername";
-$adminPass = password_hash("yourpw", PASSWORD_DEFAULT);
+$adminUser = "wontleaklmao";
+$adminPass = password_hash("nicetrylmao", PASSWORD_DEFAULT);
 
 $conn->query("INSERT INTO admins (username, password) 
               VALUES ('$adminUser', '$adminPass') 
@@ -60,4 +60,5 @@ $conn->query("INSERT INTO admins (username, password)
 echo "Admin bamdev2025 has been added successfully!";
 $conn->close();
 ?>
+
 
